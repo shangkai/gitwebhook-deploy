9620c69e1b3cfee478dfb54ab2aaba9823981d1c
ShangKai <zzshangkai@gmail.com>
2016-03-13 12:53:48 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
 
-echo "Start deployment"
+if [ ! -n "$1" ]; then
+  echo "required parameter missing."
+  exit 1
+fi
+
+if [ ! -d "$1"]; then
+    echo "$1 director is not exist"
+    exit 1
+fi
+
+cd $0
+
+if [ $? != 0 ]; then
+    echo "$1 Permission denied"
+    exit 1
+fi
+
+echo "start deploy..."
+
+#git add . -A && git stash
+git pull origin master
+
+if [ $? != 0 ]; then
+    echo "$1 git pull error"
+    exit 1
+fi
+
+echo "deploy finished."
+exit 0
